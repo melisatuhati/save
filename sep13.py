6313cd1f4d39ecd3b37411a7b6d78cfe2d63ee5a
melisatuhati <meli.erwasih@andikomalboro.shop>
2024-12-23 04:36:23 +0700

    Update sep13.py

--- sep13.py
+++ sep13.py
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/7a9eab79-1586-4160-a284-72cce30f8766")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/0792efc9-577a-439a-909b-acfca0d87747")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
