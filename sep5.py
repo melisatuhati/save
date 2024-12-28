4ef9c7faa93593b00e30521f71dc957074bc0463
melisatuhati <meli.erwasih@andikomalboro.shop>
2024-12-28 14:52:04 +0700

    Update sep5.py

--- sep5.py
+++ sep5.py
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/3462de7b-9ea4-4673-b4eb-43848e255209")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/0a9d94c8-13c2-49f7-a11c-0068bf21fb79")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
