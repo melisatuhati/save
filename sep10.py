80b68a694249aa1d4fe3bf5079632d35fc33fe51
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-01-01 00:21:58 +0700

    Update sep10.py

--- sep10.py
+++ sep10.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://fa393d597a413db3ac0f__cr.pl:e1b07fba707201aa@gw.dataimpulse.com:10011',
-        'https': 'https://fa393d597a413db3ac0f__cr.pl:e1b07fba707201aa@gw.dataimpulse.com:10011',
+        'http': 'http://geonode_NdOwa03wsf:77c77434-436d-4e78-a435-444207a82f5c@premium-residential.geonode.com:9010',
+        'https': 'https://geonode_NdOwa03wsf:77c77434-436d-4e78-a435-444207a82f5c@premium-residential.geonode.com:9010',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
