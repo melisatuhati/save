7bae7e8a411d3eabdecad8531e0f21ebcae4384c
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-01-11 17:08:37 +0700

    Update sep7.py

--- sep7.py
+++ sep7.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_yYDpudUMzO:4f977a1e-723c-4de4-9053-d392e9bd5cdc@premium-residential.geonode.com:9007',
-        'https': 'https://geonode_yYDpudUMzO:4f977a1e-723c-4de4-9053-d392e9bd5cdc@premium-residential.geonode.com:9007',
+        'http': 'http://b25c99f83a18958c69bd__cr.br:b12da4e3f444fcc0@gw.dataimpulse.com:10007',
+        'https': 'https://b25c99f83a18958c69bd__cr.br:b12da4e3f444fcc0@gw.dataimpulse.com:10007',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
