c0ddc6fee5347941b13e58e3c51e49f7f25a6140
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-01-14 16:42:37 +0700

    Update sep8.py

--- sep8.py
+++ sep8.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://98ad0b46940a64f9ee4d__cr.pl:42976f1d08e4b021@gw.dataimpulse.com:10008',
-        'https': 'https://98ad0b46940a64f9ee4d__cr.pl:42976f1d08e4b021@gw.dataimpulse.com:10008',
+        'http': 'http://98ad0b46940a64f9ee4d__cr.pl:42976f1d08e4b021@gw.dataimpulse.com:10018',
+        'https': 'https://98ad0b46940a64f9ee4d__cr.pl:42976f1d08e4b021@gw.dataimpulse.com:10018',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
