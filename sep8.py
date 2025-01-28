b85f9f26a2100a44d594ebf254df938227e8da01
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-01-28 17:00:51 +0700

    Update sep8.py

--- sep8.py
+++ sep8.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/7290f538-9144-4bff-9a9b-b8f56feb1ded")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/6bf10b74-0b59-430e-9926-b0a0be52946c")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
