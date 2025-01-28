78c8af12a3652ae4aeda9818aae34510f749d2ab
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-01-28 19:17:43 +0700

    Update sep6.py

--- sep6.py
+++ sep6.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://9642d59ea5fcf86bff73__cr.th:7d5259472a44d380@gw.dataimpulse.com:10006',
-        'https': 'http://9642d59ea5fcf86bff73__cr.th:7d5259472a44d380@gw.dataimpulse.com:10006',
+        'http': 'http://9642d59ea5fcf86bff73__cr.th:7d5259472a44d380@gw.dataimpulse.com:10106',
+        'https': 'http://9642d59ea5fcf86bff73__cr.th:7d5259472a44d380@gw.dataimpulse.com:10106',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
