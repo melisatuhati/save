946ab2b6053d527fb922d22711540c54ad2672a6
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-02-01 17:14:50 +0700

    Update sep4.py

--- sep4.py
+++ sep4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://a476bf5bc1be0e676f86__cr.au:f28741e71c788dd5@gw.dataimpulse.com:10104',
-        'https': 'https://a476bf5bc1be0e676f86__cr.au:f28741e71c788dd5@gw.dataimpulse.com:10104',
+        'http': 'http://a476bf5bc1be0e676f86__cr.as:f28741e71c788dd5@gw.dataimpulse.com:10114',
+        'https': 'https://a476bf5bc1be0e676f86__cr.as:f28741e71c788dd5@gw.dataimpulse.com:10114',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
