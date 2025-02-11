448f0138cb194d4653f3d3d885a1a8441ff71817
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-02-11 09:26:47 +0700

    Update sep13.py

--- sep13.py
+++ sep13.py
@@ -1,3 +1,4 @@
+
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -5,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://9828b7f9f6ed655b11a9__cr.au:89940e5b7a41001c@gw.dataimpulse.com:10013',
-        'https': 'https://9828b7f9f6ed655b11a9__cr.au:89940e5b7a41001c@gw.dataimpulse.com:10013',
+        'http': 'http://e62df2b45d8538038cc1__cr.jm:0a9fd0bf04b1d648@gw.dataimpulse.com:10013',
+        'https': 'https://e62df2b45d8538038cc1__cr.jm:0a9fd0bf04b1d648@gw.dataimpulse.com:10013',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -18,16 +19,21 @@ chrome_options.add_argument("--window-size=1920, 1200")
 # Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--disable-background-networking")  # Mencegah koneksi jaringan di latar belakang
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument("--disable-client-side-phishing-detection")  # Nonaktifkan deteksi phishing
+chrome_options.add_argument("--disable-default-apps")  # Nonaktifkan aplikasi bawaan Chrome
+chrome_options.add_argument("--disable-features=NetworkPrediction")  # Nonaktifkan prediksi jaringan
+chrome_options.add_argument("--disable-sync")  # Nonaktifkan sinkronisasi
+chrome_options.add_argument("--metrics-recording-only")  # Nonaktifkan pengumpulan data
+chrome_options.add_argument("--safebrowsing-disable-auto-update")  # Nonaktifkan pembaruan otomatis Safe Browsing
+chrome_options.add_argument("--disable-component-update")  # Nonaktifkan pembaruan komponen
+chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keandalan domain
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get('https://httpbin.org/ip')
-print(driver.page_source)
-
-time.sleep(1)
-
-driver.get("https://sepolia-faucet.pk910.de/#/mine/a4a5ce3b-ae06-4da5-ad3d-8abceb6c74c0")
-time.sleep(50)
+driver.get("https://sepolia-faucet.pk910.de/#/mine/7fdd4560-5179-41ad-be9f-9fcb9b4bde38")
+time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
 content_text = div_element.text
