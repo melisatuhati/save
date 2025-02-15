3a97667143a136939adf0fc737429e938110fcf1
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-02-16 01:55:13 +0700

    Update sep12.py

--- sep12.py
+++ sep12.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://5317b4b8f71f758a4271__cr.au:52edf9e9867412d1@gw.dataimpulse.com:10112',
-        'https': 'https://5317b4b8f71f758a4271__cr.au:52edf9e9867412d1@gw.dataimpulse.com:10112',
+        'http': 'http://00530ed4458339df9222__cr.co:a1e774c0831c3848@gw.dataimpulse.com:10012',
+        'https': 'https://00530ed4458339df9222__cr.co:a1e774c0831c3848@gw.dataimpulse.com:10012',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
