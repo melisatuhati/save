2f28c024d6a21fa87c3db106eeaf4473d06347bf
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-02-19 01:01:30 +0700

    Update sep2.py

--- sep2.py
+++ sep2.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://30dfdfd0b19823b594a9__cr.us:807f3930f59ec3af@gw.dataimpulse.com:10002',
-        'https': 'https://30dfdfd0b19823b594a9__cr.us:807f3930f59ec3af@gw.dataimpulse.com:10002',
+        'http': 'http://eeaf7ae099068c3b552e__cr.us:24adce5c66339ae3@gw.dataimpulse.com:10002',
+        'https': 'https://eeaf7ae099068c3b552e__cr.us:24adce5c66339ae3@gw.dataimpulse.com:10002',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/f6fb6064-8abf-4f78-9667-f3ab92f7da09")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/ea85af23-444f-4c24-8173-b4a92e4d0600")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
