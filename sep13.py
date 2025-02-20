48789d4dfdd722dd9ea5bf9bbe60005f81401f21
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-02-20 17:35:28 +0700

    Update sep13.py

--- sep13.py
+++ sep13.py
@@ -6,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://e78f029e08ed75b75ba9__cr.fr:be62433ef887d205@gw.dataimpulse.com:10123',
-        'https': 'https://e78f029e08ed75b75ba9__cr.fr:be62433ef887d205@gw.dataimpulse.com:10123',
+        'http': 'http://e78f029e08ed75b75ba9__cr.sg:be62433ef887d205@gw.dataimpulse.com:10013',
+        'https': 'https://e78f029e08ed75b75ba9__cr.sg:be62433ef887d205@gw.dataimpulse.com:10013',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -19,21 +19,16 @@ chrome_options.add_argument("--window-size=1920, 1200")
 # Hapus '--headless' untuk melihat apakah ekstensi berjalan dengan benar dalam mode normal
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument("--disable-background-networking")  # Mencegah koneksi jaringan di latar belakang
-chrome_options.add_argument('--disable-extensions')
-chrome_options.add_argument("--disable-client-side-phishing-detection")  # Nonaktifkan deteksi phishing
-chrome_options.add_argument("--disable-default-apps")  # Nonaktifkan aplikasi bawaan Chrome
-chrome_options.add_argument("--disable-features=NetworkPrediction")  # Nonaktifkan prediksi jaringan
-chrome_options.add_argument("--disable-sync")  # Nonaktifkan sinkronisasi
-chrome_options.add_argument("--metrics-recording-only")  # Nonaktifkan pengumpulan data
-chrome_options.add_argument("--safebrowsing-disable-auto-update")  # Nonaktifkan pembaruan otomatis Safe Browsing
-chrome_options.add_argument("--disable-component-update")  # Nonaktifkan pembaruan komponen
-chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keandalan domain
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
+driver.get('https://httpbin.org/ip')
+print(driver.page_source)
+
+time.sleep(1)
+
 driver.get("https://sepolia-faucet.pk910.de/#/mine/0b7e835e-64c2-41c9-9e6e-493aed5eda92")
-time.sleep(50000)
+time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
 content_text = div_element.text
