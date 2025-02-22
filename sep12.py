9d8d3bf9e1b68218635e17f1b7061f95407eb17f
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-02-22 09:39:08 +0700

    Update sep12.py

--- sep12.py
+++ sep12.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://c0404cbc2b9e9db45588__cr.us:ef8c163abb263b3b@gw.dataimpulse.com:10012',
-        'https': 'https://c0404cbc2b9e9db45588__cr.us:ef8c163abb263b3b@gw.dataimpulse.com:10012',
+        'http': 'http://c0404cbc2b9e9db45588__cr.us:ef8c163abb263b3b@gw.dataimpulse.com:10112',
+        'https': 'https://c0404cbc2b9e9db45588__cr.us:ef8c163abb263b3b@gw.dataimpulse.com:10112',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/a9ab551b-e6d4-4e39-8251-426dc399eee9")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/da32d78d-7bc8-4f83-8706-6639f88737db")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
