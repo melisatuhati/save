ffa7b1a6ab5748053f5c5c72e6713ab7b79e77ac
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-03-16 00:19:42 +0700

    Update sep12.py

--- sep12.py
+++ sep12.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://sodagar:FGAMRHJ-QQHC7SJ-0RPJ3H1-BOMNLIP-WKACAQO-GWSCKMZ-SET2MG4@unmetered.residential.proxyrack.net:10144',
-        'https': 'https://sodagar:FGAMRHJ-QQHC7SJ-0RPJ3H1-BOMNLIP-WKACAQO-GWSCKMZ-SET2MG4@unmetered.residential.proxyrack.net:10144',
+        'http': 'http://8c41ef5ecc967dff1163__cr.us:14883ab8d21301ee@gw.dataimpulse.com:10012',
+        'https': 'https://8c41ef5ecc967dff1163__cr.us:14883ab8d21301ee@gw.dataimpulse.com:10012',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -26,7 +26,7 @@ print(driver.page_source)
 
 time.sleep(1)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/e6cd471b-85ae-4242-b724-67eb41222bd2")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/524552f0-c3b9-463a-a8be-4fcc27d38f07")
 time.sleep(50)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
