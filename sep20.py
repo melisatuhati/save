67c240dfdeff8ef28929007d0314f2a2cc3967df
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-03-17 03:48:13 +0700

    Update sep20.py

--- sep20.py
+++ sep20.py
@@ -6,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10020',
-        'https': 'https://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10020',
+        'http': 'http://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10055',
+        'https': 'https://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10055',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
