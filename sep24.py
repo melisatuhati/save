80667b739129c4b4e2f5219cb34d7d8707d646da
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-03-17 04:09:44 +0700

    Update sep24.py

--- sep24.py
+++ sep24.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://8c41ef5ecc967dff1163__cr.jm:14883ab8d21301ee@gw.dataimpulse.com:10024',
-        'https': 'https://8c41ef5ecc967dff1163__cr.jm:14883ab8d21301ee@gw.dataimpulse.com:10024',
+        'http': 'http://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10024',
+        'https': 'https://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10024',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/ca6bc87a-f7d0-4a3b-8ca6-ba4f5ddf7eaf")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/0d21bf5f-4183-4281-8415-066b88923f6e")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
