f79b9c1073356fb4eddd2625da2e5413604f6e07
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-03-19 01:42:37 +0700

    Update sep19.py

--- sep19.py
+++ sep19.py
@@ -6,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10019',
-        'https': 'https://geonode_4FVjCowjg9:a2a43a41-1308-43c8-afb5-aa18134224bc@proxy.geonode.io:10019',
+        'http': 'http://geonode_y9cN7qjfl0:57b054b4-a149-497e-b98c-9dae574bfbfc@proxy.geonode.io:10019',
+        'https': 'https://geonode_y9cN7qjfl0:57b054b4-a149-497e-b98c-9dae574bfbfc@proxy.geonode.io:10019',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
@@ -32,7 +32,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/811273bc-0fe9-40d4-86a5-652866ba3c1a")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/07ac9399-b027-4f0b-b9f5-b27d70b05081")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
