925651ace4dd74e0c104f3f8f4ad18e8387c8347
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-05-06 19:26:38 +0700

    Update sep4.py

--- sep4.py
+++ sep4.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://geonode_GLuw0i31IU:be094225-8a12-46fa-92f2-15cc3004cebf@sg.proxy.geonode.io:10074',
-        'https': 'https://geonode_GLuw0i31IU:be094225-8a12-46fa-92f2-15cc3004cebf@sg.proxy.geonode.io:10074',
+        'http': 'http://84ca4397399652bebf8d__cr.it:659117267ab7a92e@gw.dataimpulse.com:10004',
+        'https': 'https://84ca4397399652bebf8d__cr.it:659117267ab7a92e@gw.dataimpulse.com:10004',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
