db142fb77169840f42a509041f747265a0592d39
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-05-08 11:17:52 +0700

    Update sep5.py

--- sep5.py
+++ sep5.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://6f075b021b07f534ff5c__cr.it:239e32a8f507591f@gw.dataimpulse.com:10005',
-        'https': 'https://6f075b021b07f534ff5c__cr.it:239e32a8f507591f@gw.dataimpulse.com:10005',
+        'http': 'http://6f075b021b07f534ff5c__cr.it:239e32a8f507591f@gw.dataimpulse.com:10025',
+        'https': 'https://6f075b021b07f534ff5c__cr.it:239e32a8f507591f@gw.dataimpulse.com:10025',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
