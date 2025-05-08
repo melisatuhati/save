120da0783c181d935b84160b488465369de84210
melisatuhati <meli.erwasih@andikomalboro.shop>
2025-05-09 04:12:02 +0700

    Update sep14.py

--- sep14.py
+++ sep14.py
@@ -6,8 +6,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://6f075b021b07f534ff5c__cr.co:239e32a8f507591f@gw.dataimpulse.com:10014',
-        'https': 'https://6f075b021b07f534ff5c__cr.co:239e32a8f507591f@gw.dataimpulse.com:10014',
+        'http': 'http://bcdffdd3446549c1b487__cr.co:fa35d3e25afda39d@gw.dataimpulse.com:10014',
+        'https': 'https://bcdffdd3446549c1b487__cr.co:fa35d3e25afda39d@gw.dataimpulse.com:10014',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
